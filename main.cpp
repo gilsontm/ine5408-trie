0e657d36d9f62427b977ffb27d7f1b0a1daced13
Gilson <gilson.t.magro@gmail.com>
2019-11-25 15:26:31 -0200

    Documentation added.

--- main.cpp
+++ main.cpp
@@ -5,62 +5,94 @@
 using namespace std;
 using namespace structures;
 
-bool validateWord(string word) {
-    for (char c : word) {
-        if (c < 97 || c > 122) {
-            cout << word << endl;
-            return false;
+
+//! Namespace que engloba as funções do trabalho.
+/*!
+    O namespace functions engloba as funções buildTrie e validateWord,
+    utilizadas no trabalho.
+*/
+namespace functions {
+    //! Função que valida uma palavra encontrada.
+    /*!
+        \param word uma string para ser verificada.
+        \return um boolean: true caso a palavra seja válida, false caso contrário.
+
+        Para o caso de dicionários que não seguem o padrão de que
+        palavras-chave, entre colchetes, devem conter somente caracteres 
+        de 'a' a 'z' (97 a 122 em ASCII), se faz necessário validar
+        que as palavras encontradas pela função buildTrie realmente
+        cumpram essa especificação.
+    */
+    bool validateWord(string word) {
+        for (char c : word) {
+            if (c < 97 || c > 122) {
+                // cout << word << endl;
+                return false;
+            }
         }
+        return true;
     }
-    return true;
-}
 
-Trie* buildTrie(string filename) {
-    ifstream file;
-    file.open(filename);
+    //! Função que instancia e constroi uma árvore Trie a partir de um arquivo dicionário.
+    /*!
+        \param filename uma string que representa o caminho para o arquivo dicionário.
+        \return um ponteiro para uma árvore Trie, construída a partir do dicionário especificado.
 
-    string line;
-    string stream = "";
-    string word = "";
-    long begin = -1;
-    unsigned long current = 0;
+        Essa função percorre o arquivo dicionário, buscando encontrar palavras-chave
+        entre colchetes, seguidas de suas definições, como em um dicionário.
+        Ao encontrar uma palavra, ela é validada pela função validateWord, e caso
+        seja válida, sua posição e o tamanho da sua linha são inseridos na Trie.
+    */
+    Trie* buildTrie(string filename) {
+        ifstream file;
+        file.open(filename);
 
-    Trie* trie = new Trie();
+        string line;
+        string stream = "";
+        string word = "";
+        long begin = -1;
+        unsigned long current = 0;
 
-    while (getline(file, line)) {
-        for (char c : line) {
-            if (c == '[') {
-                if (begin >= 0) {
-                    if (validateWord(word))
-                        trie->insert(word.c_str(), begin, current - begin - 1);
+        Trie* trie = new Trie();
+
+        while (getline(file, line)) {
+            for (char c : line) {
+                if (c == '[') {
+                    if (begin >= 0) {
+                        if (validateWord(word))
+                            trie->insert(word.c_str(), begin, current - begin - 1);
+                    }
+                    begin = current;
+                    stream = "";
+                } else if (c == ']') {
+                    word = stream;
+                } else {
+                    stream += c;
                 }
-                begin = current;
-                stream = "";
-            } else if (c == ']') {
-                word = stream;
-            } else {
-                stream += c;
+                current++;
             }
             current++;
         }
-        current++;
+        if (validateWord(word))
+            trie->insert(word.c_str(), begin, current - begin - 1);
+        file.close();
+        return trie;
     }
-    if (validateWord(word))
-        trie->insert(word.c_str(), begin, current - begin - 1);
-    file.close();
-    return trie;
-}
+}  // namespace functions
+
+
+using namespace functions;
 
 int main() {
     string filename;
     string word;
     Data data;
 
-    cin >> filename;  // entrada
+    cin >> filename;
 
     Trie* trie = buildTrie(filename);
     
-    while (1) {  // leitura das palavras ate' encontrar "0"
+    while (1) {
         cin >> word;
 
         if (word.compare("0") == 0) {
--- trie.cpp
+++ trie.cpp
@@ -1,8 +1,20 @@
 #include <stdexcept>  // C++ Exceptions
 #include <cstring>
 
+//! Namespace que engloba as estruturas do trabalho.
+/*!
+    O namespace structures engloba a struct Data e a classe Trie.
+ */
 namespace structures {
 
+    //! Struct que encapsula o retorno do método find da classe Trie.
+    /*!
+        Contém os campos position, length e found. O atributo position
+        representa a posição da palavra no dicionário. Length representa
+        o comprimento da linha em que a palavra se encontra, e found é
+        um valor booleano que determina se a palavra foi encontrada na
+        árvore ou não.
+     */
     struct Data {
         Data() {
             position = length = 0;
@@ -20,6 +32,21 @@ namespace structures {
         bool found;
     };
 
+
+    //! Classe que implementa a árvore de prefixos "retrieval".
+    /*!
+        A árvore Trie é implementada de forma a cada nó possuir até
+        26 filhos (um para cada letra do alfabeto). O nó raiz é usado
+        como um nó sentinela, que apenas faz a ligação com os outros nós
+        da árvore, e não representa nenhum caracter.
+        Percorrer a árvore numa certa ordem de filhos (0 a 25) equivale
+        a percorrer as letras ('a' a 'z') de uma palavra. Desse modo, é 
+        possível indexar o final de palavras como os nós mais inferiores
+        da árvore. Quando uma palavra é inserida na árvore, um caminho 
+        de nós é criado para representar aquela palavra. No último nó
+        desse caminho, são guardadas as informações acerca da posição
+        e do tamanho da linha da palavra no dicionário.
+    */
     class Trie {
      public:
         Trie() {
@@ -31,6 +58,25 @@ namespace structures {
                 delete root_;
         }
 
+
+        //! Método que insere palavras na árvore.
+        /*!
+            \param word Uma sequência de caracteres que representa a palavra a ser inserida.
+            \param position A posição da palavra no dicionário.
+            \param length O tamanho da linha em que a palavra se encontra.
+
+            Verifica se a palavra não é vazia. Se for, lança uma exceção;
+            caso contrário, chama o método insert do nó raiz.
+            O método find do nó navega pela árvore, criando filhos quando
+            necessário, para que ao final se tenha um caminho de nós que 
+            representa a palavra que foi inserida.
+            Consiste em diminuir a palavra em um caracter a cada chamada, 
+            e passar a palavra reduzida para um filho, que executa o
+            mesmo processo até a palavra ter tamanho zero. Quando não há
+            mais caracteres na palavra, significa que aquele é o último
+            nó do caminho, e as informações de posição e comprimento devem
+            ser armazenadas ali.
+        */
         void insert(const char* word, unsigned long position, unsigned long length) {
             if (strlen(word) == 0)
                 throw std::runtime_error("invalid string");
@@ -40,13 +86,27 @@ namespace structures {
             root_->insert(word, position, length);
         }
 
+
+        //! Método que busca palavras na árvore.
+        /*!
+            Checa se o nó raiz existe, e se existir, chama o método find
+            do nó raiz. Cada nó, por sua vez, chama o método find em um de
+            seus filhos, reduzindo a palavra a cada chamada.
+            Se a palavra chegar ao tamanho zero, o último nó do caminho 
+            foi encontrado, e portanto a palavra foi encontrada na árvore.
+            Nesse caso, retorna-se a posição e o comprimento armazenados
+            naquele nó.
+            Se não houverem filhos suficientes para continuar no caminho
+            especificado pela palavra, então a palavra não está presente
+            na árvore.
+        */
         Data find(const char* word) {
             if (root_ == nullptr) 
                 return Data();
-            return root_->find(word);            
+            return root_->find(word);
         }
 
-     private:        
+     private:
         class Node {
         public:
             Node() { 
